fc9732eda77a1ee951b2d498dc3951c0c6ce1713
Gunnar Fandrich <gunnarfandrich@ufl.edu>
2024-04-29 14:44:21 -0400

    Output everything to a pandas dataframe

--- metrics.py
+++ metrics.py
@@ -1,15 +1,15 @@
 # Python Script to extract features from a given python file or directory
 # March 9 2024, By: Gunnar Fandrich
-# Last Updated: March 29, 2024
+# Last Updated: March 9, 2024
 
 #import pyverilog
 import sys
 import os
-
+import pandas as pd
+import numpy as np
 
 
 helpMessage = "Pass the file or directory name to be evaluated following the name of the python file!"
-
 
     
 # Function definitions
@@ -19,6 +19,8 @@ def Avg(lst):
     else:
         return round(sum(lst) / len(lst))
 
+
+# Supported File Extensions
 supportedExtensions = [
                         ".vhd",
                         ".v"
@@ -69,8 +71,9 @@ conditionalOperators = [
 
 
 
+
 # check each line for the desired operators
-def getFeatures(name):
+def getFeatures(name, fileName):
     with open(name) as fp:
     
         # initialize empty dictionaries to store counts of each target feature
@@ -118,7 +121,8 @@ def getFeatures(name):
                             operatorDict[i] = operatorDict.get(i,0) + 1
                             
                             #print(operatorDict[i])
-                            
+                    #else:
+                    #    operatorDict[i] = 0
                 
                 # Number of conditional blocks
                 for i in conditionalOperators:
@@ -129,7 +133,8 @@ def getFeatures(name):
                             
                             # add i to conditionalDict and increment its value
                             conditionalDict[i] = conditionalDict.get(i,0) + 1
-                            
+                    #else:
+                    #    conditionalDict[i] = 0       
                             
                             
 
@@ -138,14 +143,31 @@ def getFeatures(name):
     #for i in range(len(sequentialStateList)):
     #    print(sequentialStateList[i])
 
-    averageSequentialStates = Avg(sequentialStateList)
+    #averageSequentialStates = Avg(sequentialStateList)
 
 
 
 
-    print("Average Number of Sequential States: ", averageSequentialStates)
-    print("Type and Number of Arithmetic Operations: ", operatorDict)
-    print("Type and Number of Conditional Blocks: ", conditionalDict, "\n")
+    #print("Average Number of Sequential States: ", averageSequentialStates)
+    #print("Type and Number of Arithmetic Operations: ", operatorDict)
+    #print("Type and Number of Conditional Blocks: ", conditionalDict, "\n")
+    
+    
+    dfTest = (pd.DataFrame.from_dict(orient='index', columns=[fileName.split(".")[0]], data=operatorDict)).T
+    dfTest1 = (pd.DataFrame.from_dict(orient='index', columns=[fileName.split(".")[0]], data=conditionalDict)).T
+    
+    
+    #print(dfTest)
+    #print(dfTest1)
+    
+    df_merged = pd.concat([dfTest, dfTest1], axis=1)
+    df_merged['Seq. States'] = Avg(sequentialStateList)
+    
+    
+    #print(df_merged)
+    
+    return df_merged
+    
     
     
     
@@ -166,18 +188,24 @@ if name == "-?" or name == "-help" or name == "--help":
 if name == "-d" or name == "--d" or name == "-directory":
     directory = sys.argv[2]
     
+    #create empty dataframe for storage
+    df = pd.DataFrame()
+    
     for root, dirs, files in os.walk(directory):
         for file in files:
             if os.path.splitext(file)[1] in supportedExtensions:
-                print("Current File: " + os.path.join(root, file))
+                #print("Current File: " + os.path.join(root, file))
                 
-                getFeatures(os.path.join(root, file))
-
-
+                df = pd.concat([df, getFeatures(os.path.join(root, file), file)])
+                
+                # replace NaN with zeroes
+                df = df.replace(np.nan, 0)
+                
+                #print(df)
+                
     
-    #for dirname in os.listdir(directory):
-    #    for subFile in os.listdir(directory + "/" + dirname):
-    #        if ".v" in subFile:
-    #            name = directory + "/" + dirname + "/" + subFile
-    #            print(name)
-    #            getFeatures(name)
+    #integers only in dataframe
+    df = df.apply(np.int64)
+    
+    
+    print(df)
